4e178dccd06fd368af99fb4c4c2a6d9958e6856d
Rachid amhouch <rachid.amhouch@gmail.com>
2023-06-21 00:41:29 +0100

    anything

--- pstr.c
+++ pstr.c
@@ -11,7 +11,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *head = *stack;
 
 	(void)line_number;
-	while (head)
+	while (head && head->n > 0 && head->n < 128)
 	{
 		dprintf(1, "%c", head->n);
 		head = head->next;
